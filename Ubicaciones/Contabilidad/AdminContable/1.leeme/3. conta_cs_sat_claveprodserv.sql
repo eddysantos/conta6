c1841e57559be85fb51e8883d2e29e3ab842a723
Eduardo Santos <e.santosancira@gmail.com>
2018-05-07 13:07:00 -0500

    Revert "catalogo SAT cuentas y catalogo de cuentas internas"
    This reverts commit d204ffb18a17f592e06df810069b8d8deab2ddfb.

--- Ubicaciones/Contabilidad/AdminContable/1.leeme/3. conta_cs_sat_claveprodserv.sql
+++
@@ -1,5 +0,0 @@
-create table conta_cs_sat_claveprodserv	(
-pk_c_ClaveProdServ	varchar	(	8	)	NOT NULL 	PRIMARY KEY	,
-s_descripcion	varchar	(	250	)	DEFAULT NULL		,
-d_fechaInicioVigencia	date				DEFAULT NULL		,
-s_activo	char	(	1	)	DEFAULT '0' );		
--- Ubicaciones/Contabilidad/AdminContable/1.leeme/4. conta_cs_sat_natur.sql
+++
@@ -1,4 +0,0 @@
-create table conta_cs_sat_natur	(
-pk_id_naturaleza	varchar	(	2	)	NOT NULL 	PRIMARY KEY	,
-s_naturaleza	varchar	(	100	)	DEFAULT NULL		,
-d_fechaInicioVigencia	timestamp				NOT NULL DEFAULT CURRENT_TIMESTAMP	);
