9210d30b3edf4b8c0128d285afd65ad6f7d197ac
adriana <admado@gmail.com>
2018-05-09 17:58:10 -0500

    admin

--- Ubicaciones/Contabilidad/AdminContable/js/AdministracionContable.js
+++ Ubicaciones/Contabilidad/AdminContable/js/AdministracionContable.js
@@ -5,81 +5,112 @@ $(document).ready(function(){
     var status = $(this).attr('status');
 
     switch (accion) {
-//CIERRE DE MES Y CATALOGO
-      case "eCap":
-      if (status == 'cerrado') {
-        $('#contornoEmp').fadeIn();
-        $('#empleadosCap').fadeIn();
-        $(this).attr('status', 'abierto');
-        $(this).css('cssText', 'color: rgb(209, 28, 28) !important');
-        $(this).css('font-size', '20px');
-      } else {
-        $('#contornoEmp').fadeOut();
-        $('#empleadosCap').fadeOut();
-        $(this).attr('status', 'cerrado');
-        $(this).css('color', "");
-        $(this).css('font-size', "");
-      }
-        break;
+	     //CIERRE DE MES Y CATALOGO
+        case "eCap":
+        if (status == 'cerrado') {
+          $('#contornoEmp').fadeIn();
+          $('#empleadosCap').fadeIn();
+          $(this).attr('status', 'abierto');
+          $(this).css('cssText', 'color: rgb(209, 28, 28) !important');
+          $(this).css('font-size', '20px');
+        } else {
+          $('#contornoEmp').fadeOut();
+          $('#empleadosCap').fadeOut();
+          $(this).attr('status', 'cerrado');
+          $(this).css('color', "");
+          $(this).css('font-size', "");
+        }
+          break;
 
-//CIERRE DE MES
-    case "generar":
-    if (status == 'cerrado') {
-      $('#contornoGen').fadeIn();
-      $(this).attr('status', 'abierto');
-      $(this).css('cssText', 'color: rgb(209, 28, 28) !important');
-      $(this).css('font-size', '20px');
-    } else {
-      $('#contornoGen').fadeOut();
-      $(this).attr('status', 'cerrado');
-      $(this).css('color', "");
-      $(this).css('font-size', "");
-    }
-      break;
-    case "modificar":
-    if (status == 'cerrado') {
-      $('#contornoMod').fadeIn();
-      $(this).attr('status', 'abierto');
-      $(this).css('cssText', 'color: rgb(209, 28, 28) !important');
-      $(this).css('font-size', '20px');
-    } else {
-      $('#contornoMod').fadeOut();
-      $(this).attr('status', 'cerrado');
-      $(this).css('color', "");
-      $(this).css('font-size', "");
-    }
-      break;
-    case "consultar":
-    if (status == 'cerrado') {
-      $('#contornoCon').fadeIn();
-      $(this).attr('status', 'abierto');
-      $(this).css('cssText', 'color: rgb(209, 28, 28) !important');
-      $(this).css('font-size', '20px');
-    } else {
-      $('#contornoCon').fadeOut();
-      $(this).attr('status', 'cerrado');
-      $(this).css('color', "");
-      $(this).css('font-size', "");
-    }
-      break;
-    case "eCap":
-    if (status == 'cerrado') {
-      $('#contornoEmp').fadeIn();
-      $('#empleadosCap').fadeIn();
-      $(this).attr('status', 'abierto');
-      $(this).css('cssText', 'color: rgb(209, 28, 28) !important');
-      $(this).css('font-size', '20px');
-    } else {
-      $('#contornoEmp').fadeOut();
-      $('#empleadosCap').fadeOut();
-      $(this).attr('status', 'cerrado');
-      $(this).css('color', "");
-      $(this).css('font-size', "");
-    }
-      break;
-        default:
-          console.error("Something went terribly wrong...");
+    		//CIERRE DE MES
+    		case "generar":
+    		if (status == 'cerrado') {
+    		  $('#contornoGen').fadeIn();
+    		  $(this).attr('status', 'abierto');
+    		  $(this).css('cssText', 'color: rgb(209, 28, 28) !important');
+    		  $(this).css('font-size', '20px');
+    		} else {
+    		  $('#contornoGen').fadeOut();
+    		  $(this).attr('status', 'cerrado');
+    		  $(this).css('color', "");
+    		  $(this).css('font-size', "");
+    		}
+    		  break;
+
+        case "modificar":
+        if (status == 'cerrado') {
+          $('#contornoMod').fadeIn();
+          $(this).attr('status', 'abierto');
+          $(this).css('cssText', 'color: rgb(209, 28, 28) !important');
+          $(this).css('font-size', '20px');
+        } else {
+          $('#contornoMod').fadeOut();
+          $(this).attr('status', 'cerrado');
+          $(this).css('color', "");
+          $(this).css('font-size', "");
+        }
+          break;
+
+        case "consultar":
+        if (status == 'cerrado') {
+          $('#contornoCon').fadeIn();
+          $(this).attr('status', 'abierto');
+          $(this).css('cssText', 'color: rgb(209, 28, 28) !important');
+          $(this).css('font-size', '20px');
+        } else {
+          $('#contornoCon').fadeOut();
+          $(this).attr('status', 'cerrado');
+          $(this).css('color', "");
+          $(this).css('font-size', "");
+        }
+          break;
+
+        case "eCap":
+        if (status == 'cerrado') {
+          $('#contornoEmp').fadeIn();
+          $('#empleadosCap').fadeIn();
+          $(this).attr('status', 'abierto');
+          $(this).css('cssText', 'color: rgb(209, 28, 28) !important');
+          $(this).css('font-size', '20px');
+        } else {
+          $('#contornoEmp').fadeOut();
+          $('#empleadosCap').fadeOut();
+          $(this).attr('status', 'cerrado');
+          $(this).css('color', "");
+          $(this).css('font-size', "");
+        }
+          break;
+            default:
+              console.error("Something went terribly wrong...");
       }
     });
 
-  });
+    $('#generarCtaMst').click(function(){
+      var ctaSAT = $('#ctaSAT').find('[select=true]').html();
+      var naturSAT = $('#naturSAT').val();
+      var tipo = $('#tipo').val();
+      var ctamaestra = $('#ctamaestra').val();
+      var concepto = $('#concepto').val();
+
+      var dataString = 'ctaSAT=' + ctaSAT + '&naturSAT=' + naturSAT + '&tipo=' + tipo + '&ctamaestra=' + ctamaestra + '&concepto=' + concepto;
+
+        $.ajax({
+    			type: "POST",
+    			url: "/conta6/Ubicaciones/Contabilidad/AdminContable/actions/agregar.php",
+    			data: dataString,
+    			success: 	function(request, settings){
+            $('#respuestaCtasMST').html(request);
+    		    /*	$('#respuestaCtasMST').html("<div id='message'></div>");
+    		        $('#message').html("<h2>Tus datos han sido guardados correctamente!</h2>")
+    		        .hide()
+    		        .fadeIn(1500, function() {
+    					         $('#message').append("<a href='index.php?action=see'>Ver usuarios registrados</a>");
+    		        });*/
+    		    }
+    		});
+
+
+    });
+
+
+});
