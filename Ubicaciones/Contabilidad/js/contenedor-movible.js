ccd5760ccf2d2df43cc219362998f4588bb98a76
Eduardo Santos <e.santosancira@gmail.com>
2018-05-16 13:19:58 -0500

    Attempt to restore.
    Attempt to restore.

--- Ubicaciones/Contabilidad/js/contenedor-movible.js
+++
@@ -1,9 +0,0 @@
-var lastIndex = 0;
-$("li").on("click", function(){
-  var difference = Math.abs($(this).index() - lastIndex);
-  $(".contenedor-movible").css({
-    "transition": "transform " + difference * 300 + "ms ease-in-out",
-    "transform": "translateX(" + $(this).index() * -100 + "%)"
-  });
-  lastIndex = $(this).index();
-})
