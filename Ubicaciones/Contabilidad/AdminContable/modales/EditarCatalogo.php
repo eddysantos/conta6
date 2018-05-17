816452d97394153f79ef27e18c2bf76064fd9dad
epinales <ae_pinales@hotmail.com>
2018-05-17 12:18:53 -0500

    Modificaciones en Admin Contable
    Nuevo modal para editar cierre de mes,
    limpieza es archivos CSS se comentaron  y eliminaron clases que no se
    utilizan
    Modificaciones en archivos Cierremes.php , corresponsales.php y en sus
    modales correspondientes

--- Ubicaciones/Contabilidad/AdminContable/modales/EditarCatalogo.php
+++ Ubicaciones/Contabilidad/AdminContable/modales/EditarCatalogo.php
@@ -15,7 +15,7 @@
               <tbody class="cuerpo">
                 <tr class="row mt-4">
                   <td class="col-md-12 input-effect">
-                    <input  list="cuentasSAT" class="text-normal efecto"  id="medit-ctaSAT">
+                    <input  list="cuentasSAT" class="efecto"  id="medit-ctaSAT">
                     <datalist id="cuentasSAT"></datalist>
                     <label for="medit-ctaSAT">CUENTAS SAT</label>
                   </td>
@@ -26,12 +26,11 @@
                     <label for="mconcepto">CONCEPTO</label>
                   </td>
                   <td class="col-md-3 input-effect">
-                    <input  list="status" class="efecto"  id="medit-status">
-                    <datalist id="status">
+                    <select class="custom-select" id="medit-status">
+                      <option selected>ESTATUS CAPTURA</option>
                       <option>Activa</option>
                       <option>Inactiva</option>
-                    </datalist>
-                    <label for="medit-status">ESTATUS CAPTURA</label>
+                    </select>
                   </td>
                   <td class="col-md-3 input-effect">
                     <input  list="NSAT" class="efecto"  id="medit-naturSAT">
