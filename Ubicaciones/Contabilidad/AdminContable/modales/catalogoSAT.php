6d9557efdc0240d761e599845d70f85491c48a7d
epinales <ae_pinales@hotmail.com>
2018-05-21 12:37:43 -0500

    Modal para Catalogo Sat
    Se realizo modal para catalogo Sat

---
+++ Ubicaciones/Contabilidad/AdminContable/modales/catalogoSAT.php
@@ -0,0 +1,44 @@
+<!--EDITAR CATALO DE CUENTAS-->
+<div class="modal fade" id="catalogoSAT" style="margin-top:50px">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button class="close" type="button" name="button" data-dismiss="modal" area-label="close">
+          <i class="fa fa-times-circle-o fa-2x" aria-hidden="true"></i>
+        </button>
+        <h5 class="modal-tittle">Catalogo SAT</h5>
+      </div>
+      <div class="modal-body p-0">
+        <div class="container-fluid">
+          <div id="" class="contorno">
+            <table class='table text-center table-hover'>
+              <thead>
+                <tr class='row encabezado'>
+                  <td class='col-md-1'>Nivel</td>
+                  <td class='col-md-1'>Codigo Agrupador</td>
+                  <td class='col-md-4'>Nombre de la Cuenta</td>
+                  <td class='col-md-1'>Activa</td>
+                  <td class='col-md-2'>Publicacion Diario Oficial</td>
+                  <td class='col-md-2'>Clasificación</td>
+                  <td class='col-md-1'>Cuenta de Balance</td>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class='row borderojo'>
+                  <td class='col-md-1'>2</td>
+                  <td class='col-md-1'>106.01</td>
+                  <td class='col-md-4'>Cuentas y documentos por cobrar a corto plazo nacional</td>
+                  <td class='col-md-1'>s</td>
+                  <td class='col-md-2'>05-01-2015</td>
+                  <td class='col-md-2'>Activos Circulantes</td>
+                  <td class='col-md-1'>Activo</td>
+                </tr>
+
+              </tbody>
+            </table>
+          </div>
+        </div><!--termina el Container-Fluid-->
+      </div><!--termina el Cuerpo del Modal-->
+    </div><!--termina el COntenido del Modal-->
+  </div>
+</div>
