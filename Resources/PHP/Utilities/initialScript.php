4609fc819383b8fcfd878885ed8f34eaabb9cbbf
epinales <ae_pinales@hotmail.com>
2018-10-03 09:52:12 -0500

    PDF de anticipo

--- Resources/PHP/Utilities/initialScript.php
+++ Resources/PHP/Utilities/initialScript.php
@@ -5,14 +5,14 @@ error_reporting(0);
 require $root . '/conta6/Resources/PHP/Utilities/session.php';
 $_SESSION['user_name'] = 'admado';
 $usuario = $_SESSION['user_name'];
-$aduana = 240;
-// $aduana = 470;
+// $aduana = 240;
+$aduana = 470;
 
 include($root . '/conta6/Resources/PHP/Databases/Conexion.php');
 date_default_timezone_set('America/Monterrey');
 
 require $root . '/conta6/Resources/PHP/actions/consultaPermisos.php';
-require $root . '/conta6/Resources/PHP/actions/consultaDatosCIA.php';
+require $root . '/Conta6/Resources/PHP/actions/consultaDatosCIA.php';
 
 function exit_script($input_array){
   $json_string = json_encode($input_array);
