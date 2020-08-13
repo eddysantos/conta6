4030c9210bc303ed367fc6f3e658ad7d49f20e4f
epinales <ae_pinales@hotmail.com>
2020-08-13 10:08:04 -0500

    conexion a SpectrumTools

---
+++ Resources/PHP/Databases/Conexion.php
@@ -0,0 +1,38 @@
+<?php
+
+//
+// $datab = 'SpectrumTools';
+// $host = 'localhost';
+// $port = 3306;
+// $usr = 'stools_connection';
+// $pwd = '$t0olsC0nn3c71on';
+
+// $datab = 'SpectrumTools';
+// $host = 'localhost';
+// // $host = '10.1.4.31';
+// $port = 3306;
+// $usr = 'stools_connection';
+// $pwd = '$t0olsC0nn3c71on';
+
+/* PUERTOS LOCALES PARA DESARROLLO */
+$datab = 'SpectrumTools';
+$host = 'localhost';
+$port = 8889;
+$usr = 'root';
+$pwd = 'root';
+
+$db = new mysqli($host, $usr, $pwd, $datab, $port) or die ('Could not connect to the database server ' . $login->error );
+
+
+
+// date_default_timezone_set('America/Monterrey');
+//
+// function exit_script($input_array){
+//   $json_string = json_encode($input_array, JSON_UNESCAPED_UNICODE);
+//   echo $json_string;
+//   global $db;
+//   $db->close();
+//   die();
+// }
+
+?>
