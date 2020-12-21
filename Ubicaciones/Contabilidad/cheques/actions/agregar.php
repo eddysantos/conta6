b1502b60d454dd39af0d0b5d62eff85f758c73c7
epinales <ae_pinales@hotmail.com>
2020-12-20 23:01:30 -0600

    .

--- Ubicaciones/Contabilidad/cheques/actions/agregar.php
+++ Ubicaciones/Contabilidad/cheques/actions/agregar.php
@@ -23,6 +23,10 @@ $idcheque_folControl = trim($_POST['idcheque_folControl']);
 $ctagastos = 0;
 $notaCred = 0;
 
+if ($gastoOficina == '') {
+  $gastoOficina = null;
+}
+
 require $root . '/Resources/PHP/actions/insertaDetalleCheque.php';
 $partida = $db->insert_id;
 
