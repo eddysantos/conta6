c0081e2908bf86a71259d1da0b04624271ef5d22
epinales <ae_pinales@hotmail.com>
2020-12-20 18:58:37 -0600

    conexion dev

--- Resources/PHP/Databases/Conexion.php
+++ Resources/PHP/Databases/Conexion.php
@@ -7,12 +7,12 @@
 // $usr = 'stools_connection';
 // $pwd = '$t0olsC0nn3c71on';
 
-// $datab = 'Dev_SpectrumTools';
-// // $host = 'localhost';
-// $host = '10.1.4.31';
-// $port = 3306;
-// $usr = 'stools_connection';
-// $pwd = '$t0olsC0nn3c71on';
+$datab = 'Dev_SpectrumTools';
+// $host = 'localhost';
+$host = '10.1.4.31';
+$port = 3306;
+$usr = 'stools_connection';
+$pwd = '$t0olsC0nn3c71on';
 
 
 /* PUERTOS LOCALES PARA DESARROLLO */
@@ -23,11 +23,11 @@
 // $pwd = 'root';
 
 /** TEST DATABASE ADRIANA **/
-$datab = 'SpectrumTools';
-$host = 'localhost';
-$port = 3306;
-$usr = 'root';
-$pwd = '';
+// $datab = 'SpectrumTools';
+// $host = 'localhost';
+// $port = 3306;
+// $usr = 'root';
+// $pwd = '';
 
 $db = new mysqli($host, $usr, $pwd, $datab, $port) or die ('Could not connect to the database server ' . $login->error );
 
